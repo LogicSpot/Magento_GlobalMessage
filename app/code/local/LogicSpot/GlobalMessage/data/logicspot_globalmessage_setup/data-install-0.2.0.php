40690f357b7593544d6cc97bdfebe08dd242b256
Kamil <kamil@logicspot.com>
2015-04-20 11:09:48 +0100

    Add custom static block in installer for demonstration purposes + bump version

---
+++ app/code/local/LogicSpot/GlobalMessage/data/logicspot_globalmessage_setup/data-install-0.2.0.php
@@ -0,0 +1,15 @@
+<?php
+
+$content = <<<EOT
+This is the test global message installed by default. You can change it in Magento Admin by going to CMS -> Static block and editing block with identifier "global_message"
+EOT;
+
+$cmsBlock = Mage::getModel('cms/block')->load('global_message', 'identifier');
+if ($cmsBlock->isObjectNew()) {
+    $cmsBlock->setIdentifier('global_message')
+        ->setStores(array(0))
+        ->setIsActive(true)
+        ->setTitle('LogicSpot GlobalMessage block')
+        ->setContent($content)
+        ->save();
+}
