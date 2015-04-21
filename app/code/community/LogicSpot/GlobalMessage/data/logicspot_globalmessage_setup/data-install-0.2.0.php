840938eb0c94fb37fa273e40f90b8a3118465d84
Kamil <kamil@logicspot.com>
2015-04-21 16:35:50 +0100

    Added missing License from install script

--- app/code/community/LogicSpot/GlobalMessage/data/logicspot_globalmessage_setup/data-install-0.2.0.php
+++ app/code/community/LogicSpot/GlobalMessage/data/logicspot_globalmessage_setup/data-install-0.2.0.php
@@ -1,4 +1,19 @@
 <?php
+/**
+ * LogicSpot_GlobalMessage
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the GNU General Public License v3.0
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.gnu.org/licenses/gpl-3.0.txt
+ *
+ * @category    LogicSpot
+ * @package     LogicSpot_GlobalMessage
+ * @copyright   Copyright (c) 2015 LogicSpot (http://www.logicspot.com)
+ * @license     http://www.gnu.org/licenses/gpl-3.0.txt GNU General Public License v3.0
+ */
 
 $content = <<<EOT
 This is the test global message installed by default. You can change it in Magento Admin by going to CMS -> Static block and editing block with identifier "global_message"
